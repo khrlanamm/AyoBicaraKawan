0322b39ab56f221e9547eecc1e80ed9e3f67cb27
Khoirul Anam <khoirulanam1604@mail.ugm.ac.id>
2025-06-04 01:09:50 +0700

    Solve unwanted padding above toolbar

--- app/src/main/java/com/khrlanamm/ayobicarakawan/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/khrlanamm/ayobicarakawan/ui/chat/ChatFragment.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.khrlanamm.ayobicarakawan.databinding.FragmentChatBinding
 
 class ChatFragment : Fragment() {
@@ -22,16 +22,25 @@ class ChatFragment : Fragment() {
         _binding = FragmentChatBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        // Gunakan factory untuk supply string dari resources
+
+        binding.toolbar.setNavigationOnClickListener {
+            // Cara 1: Menggunakan NavController (jika Anda menggunakan Jetpack Navigation)
+            findNavController().navigateUp()
+
+            // Cara 2: Menggunakan onBackPressedDispatcher (lebih umum jika tidak spesifik ke NavController)
+            // requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+
+
         val factory = ChatViewModelFactory(requireContext())
         val chatViewModel = ViewModelProvider(this, factory).get(ChatViewModel::class.java)
 
-        val textView: TextView = binding.textChat
-        chatViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
     override fun onDestroyView() {
